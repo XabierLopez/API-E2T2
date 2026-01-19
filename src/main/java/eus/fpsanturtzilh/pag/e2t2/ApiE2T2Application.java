dc3bc27fad14e7bc179343990990a40c86ac9d57
XabierL <xlopeza24@fpsanturtzilh.eus>
2026-01-19 12:42:42 +0100

    Proiektua sortu
    Spring Boot proiektua sortu Eclipsen, beharrezko menpekotasunekin.

---
+++ src/main/java/eus/fpsanturtzilh/pag/e2t2/ApiE2T2Application.java
@@ -0,0 +1,13 @@
+package eus.fpsanturtzilh.pag.e2t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiE2T2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiE2T2Application.class, args);
+	}
+
+}
---
+++ src/test/java/eus/fpsanturtzilh/pag/e2t2/ApiE2T2ApplicationTests.java
@@ -0,0 +1,13 @@
+package eus.fpsanturtzilh.pag.e2t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiE2T2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
