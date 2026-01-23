e8d00b1441a042811b76a813cacc8424b0242e87
XabierL <xlopeza24@fpsanturtzilh.eus>
2026-01-23 13:26:21 +0100

    commit

--- src/main/java/eus/fpsanturtzilh/pag/e2t2/model/Appointment.java
+++ src/main/java/eus/fpsanturtzilh/pag/e2t2/model/Appointment.java
@@ -3,13 +3,34 @@ package eus.fpsanturtzilh.pag.e2t2.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="appointments")
 public class Appointment extends Auditable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private Integer seat;
+	@Column
 	private LocalDate date;
+	@Column
 	private LocalTime start_time;
+	@Column
 	private LocalTime end_time;
+	@Column
 	private String comment;
+	@Column
 	private String name;
-	
+	@ManyToOne
+	@JoinColumn(name = "client_id", nullable = false)
+	private Client client;
 }
--- src/main/java/eus/fpsanturtzilh/pag/e2t2/model/Client.java
+++ src/main/java/eus/fpsanturtzilh/pag/e2t2/model/Client.java
@@ -1,10 +1,14 @@
 package eus.fpsanturtzilh.pag.e2t2.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,4 +27,6 @@ public class Client extends Auditable{
 	private String email;
 	@Column
 	private Boolean home_client;
+	@OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Appointment> appointments;
 }
